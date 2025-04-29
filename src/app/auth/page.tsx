de005fd78b4b637d4a420c9cd32c520f68ed6ac9
Your Name <your-email@example.com>
2025-04-29 22:04:24 +0200

    feat: Update Discord button text and add home link to logo

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -30,13 +30,15 @@ export default function AuthenticationPage() {
           <div className="absolute inset-0 bg-black/50" />
         </div>
         <div className="absolute top-10 left-10 z-20">
-          <Image
-            src="/assets/logo_productmakers.svg"
-            alt="Product Makers Logo"
-            width={75}
-            height={20}
-            className="mr-2"
-          />
+          <Link href="/">
+            <Image
+              src="/assets/logo_productmakers.svg"
+              alt="Product Makers Logo"
+              width={75}
+              height={20}
+              className="mr-2"
+            />
+          </Link>
         </div>
         <div className="absolute inset-0 z-20 flex items-center justify-center">
           <div className="text-center max-w-2xl px-4">
