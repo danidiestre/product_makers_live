4afdef9ce2d2a337a7cb482f0d407160b2d937ad
Daniel Diestre <danieldiestre@MacBookPro-Daniel.local>
2025-05-25 14:38:49 +0200

    removed migrations for sync on neon db

--- prisma/migrations/20250506170612_add_profile_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "bio" TEXT,
-ADD COLUMN     "github" TEXT,
-ADD COLUMN     "linkedin" TEXT,
-ADD COLUMN     "twitter" TEXT,
-ADD COLUMN     "website" TEXT;
